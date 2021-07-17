16bed152a37aa0f1f316d487ca7d81c325fd6c67
BarConT <diego_9deabril@hotmail.com>
2021-07-17 04:09:57 -0300

    Simple url con Hello World

--- cruddjango/urls.py
+++ cruddjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from encuestas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', admin.site.urls),
+    path('saludo/', views.saludo),
 ]
